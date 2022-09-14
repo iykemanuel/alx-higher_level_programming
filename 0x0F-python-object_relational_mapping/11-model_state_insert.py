fab6dbdd66718d793126a53208c61731ba26b9aa
iykemanuel <igweemmanuel053@gmail.com>
2022-09-14 02:42:58 +0100

    make files exec

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" script that adds the State object “Louisiana” to the database...
+    ...hbtn_0e_6_usa """
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    louisiana = State(name='Louisiana')
+    session.add(louisiana)
+    session.commit()
+    print("{}".format(louisiana.id))
+    session.close()
